a57aa8f7759d278dc2643df05e3f4672550a3bbe
Aya720 <s88.fujioka.ayano@gmail.com>
2020-11-29 12:31:26 +0900

    初期設定　修正５

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_064711) do
+ActiveRecord::Schema.define(version: 2020_11_29_032442) do
 
-  create_table "homes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
