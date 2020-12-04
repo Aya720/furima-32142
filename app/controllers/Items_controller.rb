0c0a6523707035a216e5a8028b74f94c3100888d
Aya720 <s88.fujioka.ayano@gmail.com>
2020-12-04 17:08:42 +0900

    修正

--- app/controllers/Items_controller.rb
+++ app/controllers/Items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :detail, :category_id, :state_id, :postage_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :detail, :category_id, :state_id, :postage_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
